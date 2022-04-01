e4f74e0b872221f555a4abbebdc2e60a168e5ce7
Valentin PERON <vperon@asi.fr>
2022-04-01 09:57:36 +0200

    je ne vois pas comment faire une fonction

--- cafe-with-a-vue/cli-version/src/App.vue
+++ cafe-with-a-vue/cli-version/src/App.vue
@@ -1,18 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ restaurantName }}</h1>
+		<p class="description">
+			Bienvenue dans notre café {{ restaurantName }}! Nous sommes réputés pour
+			notre pain et nos merveilleuses pâtisseries. Faites vous plaisir dès le
+			matin ou avec un goûter réconfortant. Mais attention, vous verrez qu'il
+			est difficile de s'arrêter.
+		</p>
+
+		<h2>Contactez nous</h2>
+		<p>Adresse : {{ address }}</p>
+		<p>Téléphone : {{ phone }}</p>
+		<p>Email : {{ email }}</p>
+		<p>Horaires :</p>
+		<ul>
+			<li>L-V: 06:00 à 16:00</li>
+			<li>Samedi: 07:00 à 14:00</li>
+			<li>Dimanche: 07:00 à 12:00</li>
+		</ul>
+		<footer class="footer">
+			<p>{{ copyright }}</p>
+		</footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  props: []
 }
 </script>
 
