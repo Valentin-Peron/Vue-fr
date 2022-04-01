d6999faf271eb2fe5a3a19b86274bfaaee3aba66
Valentin PERON <vperon@asi.fr>
2022-04-01 11:46:49 +0200

    correction

--- cafe-with-a-vue/cli-version/src/components/MenuItem.vue
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -1,32 +1,30 @@
-			<section class="menu">
-				<h2>Menu</h2>
-				<div v-for="item in simpleMenu" :key="item.name" class="menu-item">
-					<img
-						class="menu-item__image"
-						:src="item.image.source"
-						:alt="item.image.alt"
-					/>
-					<div>
-						<h3>{{ item.name }}</h3>
-						<p v-if="item.inStock">En stock</p>
-						<p v-else>En rupture de stock</p>
-						<div>
-							<label for="add-item-quantity"
-								>Quantité : {{ item.quantity }}</label
-							>
-							<input
-								v-model.number="item.quantity"
-								id="add-item-quantity"
-								type="number"
-							/>
-							<button @click="addToShoppingCart(item.quantity)">
-								Ajouter au panier d'achat
-							</button>
-						</div>
-					</div>
-				</div>
-			</section>
 
-			<aside class="shopping-cart">
-				<h2>Panier d'achat : {{ shoppingCart }} articles</h2>
-			</aside>
+<script>
+export default ({
+	name: "MenuItem",
+	props: [ "name", "image", "inStock", "quantity", "addToShoppingCart" ],
+	data() {
+		return {
+			myQuantity: this.quantity
+		}
+	}
+})
+</script>
+
+<template>
+	<div class="menu-item">
+		<img class="menu-item__image" :src="image.source" :alt="image.alt" />
+		<div>
+			<h3>{{ name }}</h3>
+			<p v-if="inStock">En stock</p>
+			<p v-else>En rupture de stock</p>
+			<div>
+				<label for="add-item-quantity">Quantité : {{ quantity }}</label>
+				<input v-model.number="myQuantity" id="add-item-quantity" type="number" />
+				<button @click="addToShoppingCart(myQuantity)">
+					Ajouter au panier
+				</button>
+			</div>
+		</div>
+	</div>
+</template>
